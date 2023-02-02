f6866e9a645ed33bbacf0fab6ee0a4215019fff9
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2023-02-02 21:43:47 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,18 +28,12 @@ function isCollide(snake) {
     // If you bump into yourself 
     for (let i = 1; i < snakeArr.length; i++) {
         if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
-            //Change
-            storeScore(score);
-            alert("Game Over. Press any key to play again!");
             
             return true;
         }
     }
     // If you bump into the wall
     if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
-        //Change
-        storeScore(score);
-        alert("Game Over. Press any key to play again!");
         
         return true;
     }
